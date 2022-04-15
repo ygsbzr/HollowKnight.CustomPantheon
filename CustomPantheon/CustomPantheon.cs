88f864d83bad092a7bba5fbd6a29230c5c429b7c
YG <1440412630@qq.com>
2022-04-15 20:34:12 +0800

    Fix bug about Radiance

--- CustomPantheon/CustomPantheon.cs
+++ CustomPantheon/CustomPantheon.cs
@@ -1,11 +1,14 @@
 ﻿using Modding;
 using System.Reflection;
 using System;
+using UnityEngine;
+using System.Collections;
 namespace CustomPantheon
 {
     public partial class CustomPantheon:Mod,IGlobalSettings<Setting>,ITogglableMod
     {
 		public static Setting gs = new Setting();
+		public bool isCustom = false;
         public override string GetVersion()
         {
 			return string.Concat(new string[]
@@ -25,6 +28,34 @@ namespace CustomPantheon
             UnityEngine.SceneManagement.SceneManager.activeSceneChanged += AddDoor;
             ModHooks.LanguageGetHook += ChangeDoorText;
             ModHooks.GetPlayerVariableHook += ChangeCustomDoorVar;
+            On.PlayMakerFSM.Start += ModifyRadiance;
+            On.BossSequenceController.SetupNewSequence += BossSequenceController_SetupNewSequence;
+        }
+
+        private void BossSequenceController_SetupNewSequence(On.BossSequenceController.orig_SetupNewSequence orig, BossSequence sequence, BossSequenceController.ChallengeBindings bindings, string playerData)
+        {
+            orig(sequence, bindings, playerData);
+			if (sequence.achievementKey == "")
+			{
+				isCustom = true;
+			}
+			else
+			{
+				isCustom = false;
+			}
+		}
+
+        private void ModifyRadiance(On.PlayMakerFSM.orig_Start orig, PlayMakerFSM self)
+        {
+			orig(self);
+			if(self is
+                {
+					name:"Absolute Radiance",
+					FsmName:"Control"
+                }&&isCustom)
+            {
+				self.GetAction<SetStaticVariable>("Ending Scene", 1).setValue.boolValue = false;
+            }
         }
 
         private object ChangeCustomDoorVar(Type type, string name, object value)
@@ -64,6 +95,10 @@ namespace CustomPantheon
 			{
 				GameManager.instance.StartCoroutine(SetPantheon());
 			}
+			if(arg0.name== "GG_End_Sequence")
+            {
+				HeroController.instance.gameObject.GetComponent<MeshRenderer>().enabled = true;
+            }
 			Log("Set Custom Door");
 		}
 
